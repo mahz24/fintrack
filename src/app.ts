0202e8c6bd6fcb3e0a592f8bc70fe25125dab5b5
Mahz <marcohurtado358@gmail.com>
2026-02-02 21:34:04 -0500

    feat: helmet activate

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
